b414bea20b06d18d75354f33a39a05e84b79b074
Chow Jia Ying <chowjiaying211@gmail.com>
2021-12-03 16:39:55 +0800

    create submenu

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,22 @@
+<script>
+  import { onMount } from 'svelte';
+  import Submenu from "$lib/Submenu.svelte";
+  let projects = [];
+  onMount(async function() {
+    const res = await fetch('/projects');
+    const data = await res.json();
+    projects = data.projects;
+  });
+</script>
+<nav>
+    <Submenu name="Projects">
+      <ul>
+        {#each projects as project}
+          <li>
+            <a href={`projects/${project.id}`}>{project.name}</a>
+            <span>{project.taskCount}</span>
+          </li>
+        {/each}
+      </ul>
+  </Submenu>
+</nav>
