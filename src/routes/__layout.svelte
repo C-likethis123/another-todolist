07adeef22978bb1284a419e9a83b5bce2e1c4a63
Chow Jia Ying <chowjiaying211@gmail.com>
2021-12-04 06:03:49 +0800

    Add a sidebar for nav

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -3,7 +3,15 @@
   import Nav from "$lib/Nav.svelte";
 </script>
 
-<Nav>
-  <slot />
-</Nav>
-<slot />
+<div class="container mx-auto">
+    <div class="flex flex-row flex-wrap py-4">
+        <aside class="w-full h-screen sm:w-1/3 md:w-1/4 px-2 border-r-2">
+            <Nav>
+              <slot />
+            </Nav>
+        </aside>
+        <main role="main" class="w-full h-screen sm:w-2/3 md:w-3/4 pt-1 px-2">
+          <slot />
+        </main>
+    </div>
+</div>
