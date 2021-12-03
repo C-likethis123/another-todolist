d7b242f4537a0dce3207978e89209929a73b3730
Chow Jia Ying <chowjiaying211@gmail.com>
2021-12-04 06:24:17 +0800

    Add styling for navbar

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -6,7 +6,7 @@
     try{
       const res = await fetch('/projects');
       const data = await res.json();
-      projects = data;
+      projects = data.projects;
     } catch(err) {
       projects = [];
     }
@@ -15,28 +15,47 @@
   let newProject = '';
   let inputVisible = false;
 
-  function addProject(e) {
+  async function addProject(e) {
     if (e.charCode === 13) {
-      alert(newProject);
-      newProject = '';
-    }    
-  }
+      try {
+        const res = await fetch(`/projects`, {
+          method: 'POST',
+          body: JSON.stringify({
+            name: newProject,
+          }),
+        });
+        const {project} = await res.json();
+        projects = [...projects, project];
+        newProject = '';
+      } catch (err) {
+        alert("The project cannot be added");
+      }
+    }   
+}
 
   function showInput() {
     document.querySelector("#addProject").focus();
   }
 </script>
 
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap');
+
+h2 {
+  font-family: "Inter", "san-serif";
+}
+</style>
+
 <nav>
     <Submenu name="Projects">
-      <div class="flex">
+      <div class="flex text-4xl">
       <h2>Projects</h2>
-      <button on:click={showInput}>+</button>
+      <button on:click={showInput} class="hover:bg-gray-300 w-4 h-4.5 rounded">+</button>
       </div>
       <ul>
         {#each projects as project}
-          <li>
-            <a href={`projects/${project.id}`}>{project.name}</a>
+          <li class="hover:bg-gray-300">
+            <a href={`projects/${project._id}`}>{project.name}</a>
             <span>{project.taskCount}</span>
           </li>
         {/each}
