c28e0fc2e22de1f1e9d95c6a30cad92842ec0919
Chow Jia Ying <chowjiaying211@gmail.com>
2021-12-05 12:44:38 +0800

    Add styling for task

---
+++ src/components/Task.svelte
@@ -0,0 +1,17 @@
+<script>
+  export let title;
+  export let description;
+  export let completed;
+</script>
+
+<div class="border-2 p-2 border-solid rounded m-5">
+  <div class="flex">
+    <div>
+      <input bind:checked={completed} type="checkbox" />
+    </div>
+    <div class="px-2">
+      <div>{title}</div>
+      <div class="text-gray-500">{description}</div>
+    </div>
+  </div>
+</div>
